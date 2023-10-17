457d08dda3bfa798d2a8399356088e934ce41ed5
Noah Crouch <ncrouch1@uw.edu>
2023-10-16 17:35:10 -0700

    finished

--- app/src/main/java/com/cse475team7/datacollection/MainActivity.kt
+++ app/src/main/java/com/cse475team7/datacollection/MainActivity.kt
@@ -1,116 +1,120 @@
 package com.cse475team7.datacollection
 
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.felHR85.usbserial.UsbSerialDevice
-import com.felhr.usbserial.UsbSerialInterface
-import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileOutputStream
+import java.io.File
+import android.util.Log
+
+import com.felhr.usbserial.*
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var m_usbManager: UsbManager
-    var m_device: UsbDevice? = null
-    var m_serial: UsbSerialDevice? = null
-    var m_connection: UsbDeviceConnection? = null
-
-    val ACTION_USB_PERMISSION = "permission"
-
+    private val MYPERMISSIONSREQUESTWRITEEXTERNALSTORAGE = 123
+    private val debug = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (debug)
+            Log.e("startup", "Created View")
 
-        m_usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+        val manager: UsbManager = getSystemService (Context.USB_SERVICE) as UsbManager
+        var fos: FileOutputStream? = null
+        var serialDevice: UsbSerialDevice? = null
 
-        val filter = IntentFilter()
-        filter.addAction(ACTION_USB_PERMISSION)
-        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_ATTACHED)
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
-        registerReceiver(broadcastReceiver, filter)
-
-        on.setOnClickListener{ sendData("o") }
-        off.setOnClickListener{ sendData("x") }
-        disconnect.setOnClickListener { disconnect() }
-        connect.setOnClickListener { startUsbConnecting() }
-
-
-
-    }
-
-    private fun startUsbConnecting() {
-        val usbDevices: HashMap<String, UsbDevice>? = m_usbManager.deviceList
-        if (!usbDevices?.isEmpty()!!) {
-            var keep = true
-            usbDevices.forEach{ entry ->
-                m_device = entry.value
-                val deviceVendorId: Int? = m_device?.vendorId
-                Log.i("serial", "vendorId: "+deviceVendorId)
-                if (deviceVendorId == 1027) {
-                    val intent: PendingIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
-                    m_usbManager.requestPermission(m_device, intent)
-                    keep = false
-                    Log.i("serial", "connection successful")
-                } else {
-                    m_connection = null
-                    m_device = null
-                    Log.i("serial", "unable to connect")
-                }
-                if (!keep) {
-                    return
-                }
+        val startRecordingButton = findViewById<Button>(R.id.startRecordingButton)
+        startRecordingButton.setOnClickListener {
+            val devices = manager.deviceList
+            if (debug)
+                Log.e("init", "Init manager")
+            if (!filesDir.exists()) {
+                Log.w("making dirs", "making file path")
+                filesDir.mkdirs()
             }
-        } else {
-            Log.i("serial", "no usb device connected")
-        }
-    }
-
-    private fun sendData(input: String) {
-        m_serial?.write(input.toByteArray())
-        Log.i("serial", "sending data: "+input.toByteArray())
-    }
-
-    private fun disconnect() {
-        m_serial?.close()
-    }
-
-    private val broadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            if (intent?.action!! == ACTION_USB_PERMISSION) {
-                val granted: Boolean = intent.extras!!.getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED)
-                if (granted) {
-                    m_connection = m_usbManager.openDevice(m_device)
-                    m_serial = UsbSerialDevice.createUsbSerialDevice(m_device, m_connection)
-                    if (m_serial != null) {
-                        if (m_serial!!.open()) {
-                            m_serial!!.setBaudRate(9600)
-                            m_serial!!.setDataBits(UsbSerialInterface.DATA_BITS_8)
-                            m_serial!!.setStopBits(UsbSerialInterface.STOP_BITS_1)
-                            m_serial!!.setParity(UsbSerialInterface.PARITY_NONE)
-                            m_serial!!.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
-                        } else {
-                            Log.i("Serial", "port not open")
+            for (device:UsbDevice in devices.values) {
+                try {
+                    if (UsbSerialDevice.isSupported(device)) {
+                        var intent:PendingIntent = PendingIntent.getBroadcast(this, 0, Intent(BuildConfig.APPLICATION_ID + ".USB_PERMISSION"),
+                            PendingIntent.FLAG_IMMUTABLE)
+                        manager.requestPermission(device, intent)
+                        var filter:IntentFilter = IntentFilter(BuildConfig.APPLICATION_ID + ".USB_PERMISSION")
+                        while (!manager.hasPermission(device));
+                        if (manager.hasPermission(device)) {
+                            val connection: UsbDeviceConnection = manager.openDevice(device)
+                            serialDevice = UsbSerialDevice.createUsbSerialDevice(device, connection)
+                            val filename:String = "SensorData" + Date().toString() + ".txt"
+                            fos = FileOutputStream(File(filesDir,filename))
+                            if (debug)
+                                Log.e("serial", "Created serial and fos")
+                            break
                         }
-                    } else {
-                        Log.i("Serial", "port is null")
+
                     }
-                } else {
-                    Log.i("Serial","permission not granted")
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-            } else if (intent.action == UsbManager.ACTION_USB_DEVICE_ATTACHED) {
-                startUsbConnecting()
-            } else if (intent.action == UsbManager.ACTION_USB_DEVICE_DETACHED) {
-                disconnect()
             }
+
+
+            if (debug)
+                Log.e("reader", "created read callback")
+            if (serialDevice != null && !serialDevice!!.isOpen) {
+                serialDevice!!.open();
+                serialDevice!!.setBaudRate(115200);
+                serialDevice!!.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                serialDevice!!.setParity(UsbSerialInterface.PARITY_ODD);
+                serialDevice!!.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+            }
+            serialDevice?.read {
+                fos?.write(it)
+            }
+            if (debug)
+                Log.w("start", "start button was pressed")
+            Log.w("fos status", "fos is null? " +  (fos == null))
+        }
+        val stopRecordingButton = findViewById<Button>(R.id.stopRecordingButton)
+        stopRecordingButton.setOnClickListener {
+            if (serialDevice != null && serialDevice!!.isOpen) {
+                serialDevice!!.close()
+            }
+            if (fos != null)
+                fos!!.close()
+            if (debug)
+                Log.w("stop", "stop button was pressed")
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MYPERMISSIONSREQUESTWRITEEXTERNALSTORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can perform the file operations here
+                Toast.makeText(this,
+                    "Write Permission Granted",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                // Permission denied
+                Toast.makeText(
+                    this,
+                    "Write permission is required to save data to storage",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 }
